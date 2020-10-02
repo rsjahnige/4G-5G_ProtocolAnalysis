0ddf2c2e7f7a08bab33f001f97a776a5f31e71b6
Jason Schneck <aq79577@umbc.edu>
2020-10-02 14:30:28 -0400

    commit 2

--- 5g_aka_test.scm
+++ 5g_aka_test.scm
@@ -2,39 +2,46 @@
     (comment "First attempt for modeling 5G AKA")
 	)
 
+;;macro for AUTN
 (defmacro (AUTN sqn rand key)
     (cat (hash sqn (hash key rand)) (hash sqn rand key))
 )
 
+;;Protocol 5G-AKA 
 (defprotocol 5G-AKA basic
 
   (defrole UE
     (vars (supi RAND data) (homename servername name))
     (trace
         (send (enc supi (pubk homename)))
+        (recv (cat RAND (AUTN SQN RAND key)))
+        
 
      )
     )
 
   (defrole ServingNetwork
-    (vars (supi RAND data) (homename servername name))
+    (vars (supi RAND data) (homename servername name) (SQN xRES text) (key skey))
     (trace
         (recv (enc supi (pubk homename)))
         (send (cat (enc supi (pubk homename) servername))
-        (recv (cat RAND ))
+        (recv (cat RAND (AUTN sqn RAND key) (hash xRES RAND)))
+        (send (cat RAND (AUTN SQN RAND key))) ;; ngKSI, AMF, and ABBA need to be added???
     )
-    (non-orig seqno)
+    (non-orig SQN key)
    )
 
    (defrole HomeNetwork
-    (vars (supi RAND data) (homename servername name) (seqno text))
+    (vars (supi RAND data) (homename servername name) (SQN xRES text) (key skey))
     (trace 
         (recv (cat (enc supi (pubk homename) servername))
-        (send (cat RAND AUTN ))
+        (init xRES supi) ;;store XRES* and SUPI
+        (send (cat RAND (AUTN SQN RAND key) (hash xRES RAND)))
+        
+
     )
     (uniq-gen RAND)
-    (non-orig seqno)
-    
+    (non-orig SQN key)
    )
   )
 
@@ -51,4 +58,4 @@
 (defskeleton 
     (vars )
     (defstrandmax )
-)()
+)

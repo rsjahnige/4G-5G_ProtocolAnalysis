da7e109f05c11a07c938961468dd05f4d0faadcf
Jason Schneck <aq79577@umbc.edu>
2020-10-03 14:55:25 -0400

    commit compilies

--- 5g_aka_test.scm
+++ 5g_aka_test.scm
@@ -7,15 +7,19 @@
     (cat (hash sqn (hash key rand)) (hash sqn rand key))
 )
 
+(defmacro (RES* rand sname mac key)
+    (hash (cat (hash key rand) (hash key rand)) sname rand (hash key rand))
+)
+
 ;;Protocol 5G-AKA 
-(defprotocol 5G-AKA basic
+(defprotocol AKA5G basic
 
   (defrole UE
-    (vars (supi RAND data) (homename servername name))
+    (vars (supi RAND data) (homename servername name) (SQN text) (key skey))
     (trace
         (send (enc supi (pubk homename)))
         (recv (cat RAND (AUTN SQN RAND key)))
-        
+
 
      )
     )
@@ -24,8 +28,8 @@
     (vars (supi RAND data) (homename servername name) (SQN xRES text) (key skey))
     (trace
         (recv (enc supi (pubk homename)))
-        (send (cat (enc supi (pubk homename) servername))
-        (recv (cat RAND (AUTN sqn RAND key) (hash xRES RAND)))
+        (send (cat (enc supi (pubk homename) servername)))
+        (recv (cat RAND (AUTN SQN RAND key) (hash xRES RAND)))
         (send (cat RAND (AUTN SQN RAND key))) ;; ngKSI, AMF, and ABBA need to be added???
     )
     (non-orig SQN key)
@@ -34,8 +38,8 @@
    (defrole HomeNetwork
     (vars (supi RAND data) (homename servername name) (SQN xRES text) (key skey))
     (trace 
-        (recv (cat (enc supi (pubk homename) servername))
-        (init xRES supi) ;;store XRES* and SUPI
+        (recv (cat (enc supi (pubk homename) servername)))
+        (init (cat xRES supi)) ;;store XRES* and SUPI
         (send (cat RAND (AUTN SQN RAND key) (hash xRES RAND)))
         
 
@@ -45,17 +49,17 @@
    )
   )
 
-(defskeleton 
-    (vars )
-    (defstrandmax )
-)
+;;;(defskeleton 
+;;;    (vars )
+;;;    (defstrandmax )
+;;;)
 
-(defskeleton 
-    (vars )
-    (defstrandmax )
-)
+;;;(defskeleton 
+;;;    (vars )
+;;;    (defstrandmax )
+;;;)
 
-(defskeleton 
-    (vars )
-    (defstrandmax )
-)
+;;;(defskeleton 
+;;;    (vars )
+;;    (defstrandmax )
+;;)

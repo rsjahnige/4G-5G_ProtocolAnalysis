c4d9ca50a90ac5c102d4dabc69e2de3d54e27bcb
Jason Schneck <aq79577@umbc.edu>
2020-10-09 14:16:58 -0400

    Update SUPI and encryption

--- 5g_aka_test.scm
+++ 5g_aka_test.scm
@@ -41,6 +41,7 @@
      (recv (cat RAND (AUTN SQN RAND (ltk UE HN))))
      (send (RES* RAND SN (ltk UE HN)))
      )
+    (fn-of ("ID" (SUPI UE)))
     (non-orig (ltk UE HN))
     )
 
@@ -48,10 +49,12 @@
     (vars (SUPI RAND data) (UE SN HN name) (SQN text))
     (trace
      (recv (enc SUPI (pubk HN)))
-     (send (cat (enc SUPI (pubk HN) SN)))
+     (send (cat (enc SUPI (pubk HN)) SN))
      (recv (cat RAND (AUTN SQN RAND (ltk UE HN)) (HXRES* RAND SN (ltk UE HN))))
+    ;; (init (HXRES* RAND SN (ltk UE HN)))
      (send (cat RAND (AUTN SQN RAND (ltk UE HN)))) ;; ngKSI, AMF, and ABBA need to be added???
      (recv (RES* RAND SN (ltk UE HN)))
+     ;;(init (HRES* RAND SN (ltk UE HN)))
      (send (RES* RAND SN (ltk UE HN)))
      (recv (cat "Success" (KSEAF RAND SN SQN (ltk UE HN)) SUPI))
      )
@@ -61,7 +64,7 @@
   (defrole HomeNetwork
     (vars (SUPI RAND data) (UE SN HN name) (SQN text))
     (trace 
-     (recv (cat (enc SUPI (pubk HN) SN)))
+     (recv (cat (enc SUPI (pubk HN)) SN))
      (init (cat (XRES* RAND SN (ltk UE HN)) SUPI)) ;;store XRES* and SUPI
      (send (cat RAND (AUTN SQN RAND (ltk UE HN)) (HXRES* RAND SN (ltk UE HN))))
      (recv (RES* RAND SN (ltk UE HN)))
@@ -74,15 +77,15 @@
 
 (defskeleton AKA5G
   (vars (SUPI data) (UE HN name) (SQN text))
-  (defstrand UserEquip 3 (SUPI SUPI) (UE UE) (HN HN) (SQN SQN))
+  (defstrandmax UserEquip (SUPI SUPI) (UE UE) (HN HN) (SQN SQN))
   )
 
 (defskeleton AKA5G
   (vars (SN name))
-  (defstrand ServingNetwork 7  (SN SN))
+  (defstrandmax ServingNetwork (SN SN))
   )
 
 (defskeleton AKA5G
   (vars (SUPI RAND data) (UE HN name) (SQN text))
-  (defstrand HomeNetwork 4 (SUPI SUPI) (RAND RAND) (UE UE) (HN HN))
+  (defstrandmax HomeNetwork (SUPI SUPI) (RAND RAND) (UE UE) (HN HN))
   )
